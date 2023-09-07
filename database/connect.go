d3828688cfaf76d3843f445e49c9b6aa36e6afe9
Muhammad <autumnleafra@gmail.com>
2023-09-08 05:03:32 +0700

    first commit

---
+++ database/connect.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"log"
+	"os"
+
+	"github.com/autumnleaf-ra/go-blogger-app/models"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error load .env")
+	}
+
+	dsn := os.Getenv("DSN")
+	database, _ := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Could not connect to database")
+	} else {
+		log.Println("Connected!")
+	}
+
+	DB = database
+	database.AutoMigrate(
+		&models.User{},
+	)
+}
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/autumnleaf-ra/go-blogger-app/database"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	database.Connect()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+
+	app := fiber.New()
+	app.Listen(":" + port)
+}
